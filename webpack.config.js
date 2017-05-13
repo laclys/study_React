17e85186b98cd7ff442f5ba3450fddb6675c50e0
lac <laclys@126.com>
2017-05-13 22:12:21 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  entry: "./src/js/index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "./src/bundle.js"
+  }
+};
