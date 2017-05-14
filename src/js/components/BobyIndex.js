d7b56605563952c8e61614cf247832291b11b110
lac <laclys@126.com>
2017-05-14 21:12:41 +0800

    state

--- src/js/components/BobyIndex.js
+++ src/js/components/BobyIndex.js
@@ -1,33 +1,25 @@
 import React from 'react';
 
-export default class BobyIndex extends React.Component{
-
-    componentWillMount() {
-        console.log('body_index_componentWillMount');
+export default class BobyIndex extends React.Component {
+    constructor() {
+        super(); // 调用基类的所有初始化方法
+        this.state = {
+            username: "Lac",
+            age: 25
+        } // 自身的state只是当前作用域
     }
+    render() {
 
-    componentDidMount() {
-        console.log('body_index_componentDidMount')
-    }
-
-    render (){
-
-        var userName = 'Lac';
-        var boolInput = false;
-
-        var html01 = 'zhang\u0020yangyang' // 解析html要用unicode转码
-        var html02 = 'zhang&nbsp;yangyang' // 或 （不推荐）比较危险的形式 (容易造成xss攻击)
-
-        // comments
+        setTimeout(() => {
+            // 更改state 的时候
+            this.setState({username: "Zhangyangyang",age: 18})
+        }, 4000);
 
         return (
             <div>
                 <h2>页面主体</h2>
-                <p>{userName==''?'用户还没有登录': '用户名：' + userName}</p>
-                <p><input type="button" value = "默认按钮" disabled={boolInput}/></p>
-                {/*注释*/}
-                <p>{html01}</p>
-                <p dangerouslySetInnerHTML ={{__html : html02}}></p>
+                <p>{this.state.username}</p>
+                <p>{this.state.age}</p>
             </div>
         )
     }
