1ec33e0e58c030ffe19dc85d3bfe00d3d5f6dfb7
lac <laclys@126.com>
2017-05-14 22:03:22 +0800

    事件绑定/子组件调用父组件传递过来的事件进行传参

--- src/js/components/BobyIndex.js
+++ src/js/components/BobyIndex.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BodyChild from './bodyChild';
 
 export default class BobyIndex extends React.Component {
     constructor() {
@@ -8,12 +9,18 @@ export default class BobyIndex extends React.Component {
             age: 25
         } // 自身的state只是当前作用域
     }
+
+    changeUserInfo(age) {
+        this.setState({age: age});
+    };
+    handleChildValueChange(event) {
+        this.setState({age: event.target.value});
+    };
+
     render() {
 
-        setTimeout(() => {
-            // 更改state 的时候
-            this.setState({username: "Zhangyangyang",age: 18})
-        }, 4000);
+        // setTimeout(() => {     // 更改state 的时候     this.setState({username:
+        // "Zhangyangyang",age: 18}) }, 4000);
 
         return (
             <div>
@@ -21,6 +28,16 @@ export default class BobyIndex extends React.Component {
                 <p>{this.state.username}</p>
                 <p>{this.state.age}</p>
                 <p>{this.props.userid}</p>
+                <input
+                    type="button"
+                    value="提交"
+                    onClick={this
+                    .changeUserInfo
+                    .bind(this,18)}/>
+                <BodyChild
+                    handleChildValueChange={this
+                    .handleChildValueChange
+                    .bind(this)}/>
             </div>
         )
     }
